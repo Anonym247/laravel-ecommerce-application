2cd2ad6c423333a94de1191625628f34959338c2
Anonym247 <merdanzade2@gmail.com>
2020-01-12 03:35:07 +0400

    "Fucking Sheet"

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         \App\Providers\SettingsServiceProvider::class,
+        \App\Providers\RepositoryServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -227,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'SettingService' => \App\Providers\SettingsServiceProvider::class,
+
     ],
 
 ];
